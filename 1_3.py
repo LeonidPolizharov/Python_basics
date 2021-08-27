7a0d00146b14d4b375d240fe8c3a5bf9778f913a
LeonidPolizharov <polizharov@mail.ru>
2021-08-27 17:27:52 +0300

    excluding 'IN' operator

--- 1_3.py
+++ 1_3.py
@@ -2,7 +2,7 @@ for num in range(1, 101):
     units = num % 100 % 10
     if units == 1 and num % 100 != 11:
         print(f"{num} процент")
-    elif units in (2, 3, 4) and num % 100 not in (12, 13, 14):
+    elif (units == 2 or units == 3 or units == 4) and (num % 100 != 12 and num % 100 != 13 and num % 100 != 14):
         print(f"{num} процента")
     else:
         print(f"{num} процентов")
