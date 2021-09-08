d034f8aa60ea0d8957ed7355e9724a94d07b4a73
LeonidPolizharov <polizharov@mail.ru>
2021-09-09 00:43:57 +0300

    DZ-4

---
+++ 4_3.py
@@ -0,0 +1,39 @@
+import requests
+from datetime import datetime
+
+inp_val = input('Введите код валюты для получения текущего курса: ')
+
+
+def currency_rates(inp_arg):
+    from decimal import Decimal
+    cbr_request = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encode = requests.utils.get_encoding_from_headers(cbr_request.headers)
+    cbr_content = cbr_request.content.decode(encoding=encode)
+    parsed_cont = cbr_content.replace('>', ' ').replace('<', ' ').split()
+    # date = datetime.date(datetime.strptime(cbr_request.headers['Date'], "%a, %d %b %Y %H:%M:%S %Z"))
+    date = ''
+    for i in parsed_cont:
+        if i[0:4] == 'Date':
+            date = datetime.date(datetime.strptime(i[6:16], "%d.%m.%Y"))
+
+    if inp_arg.upper() in parsed_cont and not inp_arg.isdigit():
+        inp_arg = inp_arg.upper()
+        rate = None
+        nom = None
+        for i, val in enumerate(parsed_cont):
+            if val == inp_arg:
+                for j, sec_val in enumerate(parsed_cont[i:]):
+                    if sec_val == 'Nominal' and parsed_cont[i + j + 1][0].isdigit():
+                        nom = parsed_cont[i + j + 1]
+                    if sec_val == 'Value' and parsed_cont[i + j + 1][0].isdigit():
+                        rate = parsed_cont[i + j + 1].replace(',', '.')
+                        break
+    else:
+        return None
+    return Decimal(rate), nom, date
+
+
+if currency_rates(inp_val) is not None:
+    print(f'Курс на дату {currency_rates(inp_val)[2]}: {currency_rates(inp_val)[1]} {inp_val.upper()} = {currency_rates(inp_val)[0]} RUB')
+else:
+    print('Некорректный ввод!')
